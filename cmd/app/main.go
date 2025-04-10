c83cb55b054f87f7f28d097a2c913052c235ce04
wesleyalgorama <wesley@algorama.com.br>
2025-04-10 10:42:08 -0300

    feat(main): start server

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,55 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/wesleyalgorama/fcw/go-gateway/internal/repository"
+	"github.com/wesleyalgorama/fcw/go-gateway/internal/service"
+	"github.com/wesleyalgorama/fcw/go-gateway/internal/web/server"
+)
+
+func getEnv(key, defaultValue string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+
+	return defaultValue
+}
+
 func main() {
-	println("Hello, world!")
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		getEnv("DB_HOST", "db"),
+		getEnv("DB_PORT", "5432"),
+		getEnv("DB_USER", "user"),
+		getEnv("DB_PASSWORD", "pass"),
+		getEnv("DB_NAME", "db"),
+		getEnv("DB_SSL_MODE", "disable"),
+	)
+
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		log.Fatal("Error connecting to database: ", err)
+	}
+
+	defer db.Close()
+
+	accountRepository := repository.NewAccountRepository(db)
+	accountService := service.NewAccountService(accountRepository)
+	port := getEnv("HTTP_PORT", "8080")
+	srv := server.NewServer(accountService, port)
+	srv.ConfigureRoutes()
+
+	if err := srv.Start(); err != nil {
+		log.Fatal("Error starting server: ", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/wesleyalgorama/fcw/go-gateway
 
 go 1.24.2
 
-require github.com/google/uuid v1.6.0
+require (
+	github.com/go-chi/chi/v5 v5.2.1
+	github.com/google/uuid v1.6.0
+	github.com/joho/godotenv v1.5.1
+)
